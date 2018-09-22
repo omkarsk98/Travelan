10f1e17bcd1bf6da445af521ca65d11435c43317
programmer <omkarsk98@gmail.com>
2018-09-23 01:07:43 +0530

    Basic files made

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class AllComponents extends React.Component{
+  render() {
+    return(
+      <div>
+        Hello!
+        <br/>
+        All Components here!
+      </div>
+    );
+  }
+}
