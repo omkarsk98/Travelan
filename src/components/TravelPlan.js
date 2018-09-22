1cec8a3bd729ac1af58e8236633f193bbce55269
programmer <omkarsk98@gmail.com>
2018-09-23 02:31:10 +0530

    Welcome Page final

---
+++ src/components/TravelPlan.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const style = {
+  padding: 20,
+  margin: 20,
+  border:"solid black 2px"
+};
+
+export default class TravelPlan extends React.Component{
+  render(){
+    return(
+      <div style={style}>
+        Travel Plan Component!
+        
+      </div>
+    );
+  }
+}
