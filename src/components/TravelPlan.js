8346c1cad44be8e3f7fabec1f4367eb76fecde01
programmer <omkarsk98@gmail.com>
2018-09-23 03:06:25 +0530

    Pull Check

--- src/components/TravelPlan.js
+++ src/components/TravelPlan.js
@@ -1,17 +1,125 @@
 import React from "react";
+import {
+  Button,
+  Grid
+} from "semantic-ui-react";
 
 const style = {
   padding: 20,
   margin: 20,
-  border:"solid black 2px"
+  border: "solid black 2px"
 };
 
-export default class TravelPlan extends React.Component{
-  render(){
-    return(
+const bottomPadding = {
+  paddingBottom: 0
+};
+
+export default class TravelPlan extends React.Component {
+  render() {
+    return (
       <div style={style}>
-        Travel Plan Component!
-        
+        <h2>Travel Plan</h2>
+        {/* div for Flights */}
+        <div>
+          <h3>Flight</h3>
+          <Grid>
+            <Grid.Row style={bottomPadding}>
+              <Grid.Column mobile={8}>
+                <b>Air India</b> (Rs. 4000)
+              </Grid.Column>
+              <Grid.Column mobile={8}>
+                <Button primary type="submit">
+                  Book
+                </Button>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column mobile={8}>
+                <b>Jet Airways</b> (Rs. 4000)
+              </Grid.Column>
+              <Grid.Column mobile={8}>
+                <Button primary type="submit">
+                  Book
+                </Button>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </div>
+        <div>
+          <h3>Train</h3>
+          <Grid>
+            <Grid.Row style={bottomPadding}>
+              <Grid.Column mobile={8}>
+                <b>Rajdhani Express</b> (Rs. 2100 1st class)
+              </Grid.Column>
+              <Grid.Column mobile={8}>
+                <Button primary type="submit">
+                  Book
+                </Button>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column mobile={8}>
+                <b>Shatabdi Express</b> (Rs. 2100 1st class)
+              </Grid.Column>
+              <Grid.Column mobile={8}>
+                <Button primary type="submit">
+                  Book
+                </Button>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </div>
+        <div>
+          <h3>Bus</h3>
+          <Grid>
+            <Grid.Row style={bottomPadding}>
+              <Grid.Column mobile={8}>
+                <b>Gill Travels</b> (Rs. 2000 Sleeper)
+              </Grid.Column>
+              <Grid.Column mobile={8}>
+                <Button primary type="submit">
+                  Book
+                </Button>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column mobile={8}>
+                <b>Mahalakshmi Travels</b> (Rs. 2000 sleeper)
+              </Grid.Column>
+              <Grid.Column mobile={8}>
+                <Button primary type="submit">
+                  Book
+                </Button>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </div>
+        <div>
+          <h3>Self Drive</h3>
+          <Grid>
+            <Grid.Row style={bottomPadding}>
+              <Grid.Column mobile={8}>
+                <b>Zoom cars</b> (Rs. 3000 approx.)
+              </Grid.Column>
+              <Grid.Column mobile={8}>
+                <Button primary type="submit">
+                  Book
+                </Button>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column mobile={8}>
+                <b>Self owned</b> (Rs. 2400 approx)
+              </Grid.Column>
+              <Grid.Column mobile={8}>
+                <Button primary type="submit">
+                  Book
+                </Button>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </div>
       </div>
     );
   }
