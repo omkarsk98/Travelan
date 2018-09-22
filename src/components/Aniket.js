f7b882d7aa5c8ff265f9e9259f279f985bc960db
Aniket Kumar <aniketkumar.akr9@gmail.com>
2018-09-23 01:35:59 +0530

    Some Minor Architecture Changes

---
+++ src/components/Aniket.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Aniket extends React.Component{
+  render(){
+    return(
+      <div>
+          WIll Call my Components Here. 
+      </div>
+    );
+  }
+}
